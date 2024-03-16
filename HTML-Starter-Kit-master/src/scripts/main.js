ff92390c66f1764aabc0f42124dba2b8eedf2824
mhmdsbr <saaber.mohamad@gmail.com>
2024-03-16 13:06:18 +0330

    Feat: Popular slider section added

--- HTML-Starter-Kit-master/src/scripts/main.js
+++ HTML-Starter-Kit-master/src/scripts/main.js
@@ -2,6 +2,34 @@
 (function() {
   'use strict';
   document.addEventListener('DOMContentLoaded', function() {
-    // Your custom JavaScript goes here
+
+    // Popular Dishes Slider Pagination
+    const flickity = new Flickity('.main-carousel');
+    const carouselStatus = document.querySelector('.js-carousel-status');
+
+    function updateStatus() {
+      const slideNumber = flickity.selectedIndex + 1;
+      const totalSlides = flickity.slides.length;
+
+      const slideNumberSpan = document.createElement('span');
+      slideNumberSpan.textContent = slideNumber;
+      slideNumberSpan.classList.add('slide-number');
+
+      const totalSlidesSpan = document.createElement('span');
+      totalSlidesSpan.textContent = totalSlides;
+      totalSlidesSpan.classList.add('total-slides');
+
+      carouselStatus.innerHTML = '';
+
+      carouselStatus.appendChild(slideNumberSpan);
+      carouselStatus.appendChild(document.createTextNode('/'));
+      carouselStatus.appendChild(totalSlidesSpan);
+    }
+
+    updateStatus();
+
+    flickity.on('select', updateStatus);
+    // End
+
   });
 })();
